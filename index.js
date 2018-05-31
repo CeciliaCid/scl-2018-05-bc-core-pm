79043d3f21aebda9c4410bc871fb9b2c382fa07c
Cecilia <ccid.montero@gmail.com>
2018-05-31 15:28:02 -0400

    Declaré variables para ambas funciones

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-/* Acá va tu código */
+function encode(){
+    let mensajeEnviado = document.getElementById("textoACifrar").value;
+    let offset = parseInt(document.getElementById("keyOffset").value);
+    const resultado = window.cipher.encode(mensajeEnviado, offset);
+    document.getElementById("textoCifrado").innerHTML = resultado;
+}
+function decode(){
+    let mensajeADescifrar = document.getElementById("textoADescifrar").value;
+    let offset = parseInt(document.getElementById("keyOffset").value);
+    const resultado = window.cipher.decode(mensajeADescifrar, offset);
+    document.getElementById("textoDescifrado").innerHTML = resultado;
+}
+
